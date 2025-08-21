5e148053475af397896e08c9f38d80e44b269ba2
intaeHwang <hwang9029@ckinform.com>
2025-08-21 23:13:25 +0900

    refactor/ Money record로 수정

--- src/main/java/intae/chapter02/Money.java
+++ src/main/java/intae/chapter02/Money.java
@@ -2,14 +2,10 @@ package intae.chapter02;
 
 import java.math.BigDecimal;
 
-public class Money {
+public record Money(
+        BigDecimal amount
+) {
     public static final Money ZERO = Money.wons(0);
-
-    private final BigDecimal amount;
-
-    public Money(BigDecimal amount) {
-        this.amount = amount;
-    }
 
     public static Money wons(long amount) {
         return new Money(BigDecimal.valueOf(amount));
@@ -20,11 +16,12 @@ public class Money {
     }
 
     public Money plus(Money amount) {
-        return new Money(this.amount.add(amount.amount));
+
+        return new Money(this.amount.add(amount.amount()));
     }
 
     public Money minus(Money amount) {
-        return new Money(this.amount.subtract(amount.amount));
+        return new Money(this.amount.subtract(amount.amount()));
     }
 
     public Money times(double percent) {
@@ -33,10 +30,10 @@ public class Money {
     }
 
     public boolean isLessThan(Money other) {
-        return amount.compareTo(other.amount) < 0;
+        return amount.compareTo(other.amount()) < 0;
     }
 
     public boolean isGreaterThanOrEqual(Money other) {
-        return amount.compareTo(other.amount) >= 0;
+        return amount.compareTo(other.amount()) >= 0;
     }
 }
